f0333e57f9f620aa4d1bc643c49b0c6d045df72e
Alan Jui <alanjui.1960@gmail.com>
2016-03-05 23:55:10 +0800

    Initial project

---
+++ app/src/home/Home.js
@@ -0,0 +1,18 @@
+const MODULE_NAME = 'home';
+
+// Load the custom app ES2015 modules
+
+import HomeController from 'home/HomeController';
+
+import { ExternalLogger } from 'utils/LogDecorator';
+
+let $log = new ExternalLogger();
+    $log = $log.getInstance( "BOOTSTRAP" );
+    $log.debug( "Configuring 'users' module" );
+
+// Define the Angular 'home' module
+let module = angular.module(MODULE_NAME, [])
+  .controller('HomeController', HomeController)
+  ;
+
+export default module.name;
---
+++ app/src/main.js
@@ -0,0 +1,8 @@
+import home from 'home/Home';
+import { ExternalLogger } from 'utils/LogDecorator';
+
+let $log = new ExternalLogger();
+    $log = $log.getInstance( "BOOTSTRAP" );
+    $log.debug( "Configuring 'main' module" );
+
+export default angular.module('main', [ home ] ).name;
